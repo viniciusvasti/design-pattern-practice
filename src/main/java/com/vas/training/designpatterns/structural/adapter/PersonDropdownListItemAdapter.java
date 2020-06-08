d598b04ef877fd397f1b8eb8a2cb15febef65707
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2020-06-08 01:11:17 -0300

    Resolve PersonDropdownListItemAdapterTest

--- src/main/java/com/vas/training/designpatterns/structural/adapter/PersonDropdownListItemAdapter.java
+++ src/main/java/com/vas/training/designpatterns/structural/adapter/PersonDropdownListItemAdapter.java
@@ -1,5 +1,24 @@
 package com.vas.training.designpatterns.structural.adapter;
 
-public class PersonDropdownListItemAdapter {
+import com.vas.training.designpatterns.misc.DropdownListItem;
+import com.vas.training.designpatterns.misc.Person;
+
+public class PersonDropdownListItemAdapter implements DropdownListItem {
+
+	Person person;
+
+	public PersonDropdownListItemAdapter(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public String getText() {
+		return person.getFirstName() + " " + person.getLastName();
+	}
+
+	@Override
+	public Object getValue() {
+		return person;
+	}
 
 }

6982fc4a50160e4965bc030ac541894bf8a0b8df
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2020-06-09 20:23:51 -0300

    Implement Prototype Pattern

---
+++ src/test/java/com/vas/training/designpatterns/creational/prototype/PersonPrototypeTest.java
@@ -0,0 +1,31 @@
+package com.vas.training.designpatterns.creational.prototype;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.vas.training.designpatterns.creational.prototype.Person.Gender;
+
+class PersonPrototypeTest {
+
+	@DisplayName("Cloning Person should create a new instance of Person")
+	@Test
+	void testClonePerson() throws CloneNotSupportedException {
+		Person p1 = new Person("Jack", "Sparrow", LocalDate.now(), Gender.MALE);
+		Person p2 = p1;
+		assertTrue(p2 == p1);
+		Person p3 = p1.clone();
+		assertFalse(p3 == p1);
+
+		p2.setFirstName("Jackson");
+		assertEquals(p1.getFirstName(), p2.getFirstName());
+		assertNotEquals(p1.getFirstName(), p3.getFirstName());
+	}
+
+}
